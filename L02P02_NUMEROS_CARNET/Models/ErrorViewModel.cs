654fbdb29eedd3dc405eb82a0747259c50c2fd83
Juan Hernández <jfph25@gmail.com>
2024-04-20 08:41:07 -0600

    Add project files.

---
+++ L02P02_NUMEROS_CARNET/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace L02P02_NUMEROS_CARNET.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
